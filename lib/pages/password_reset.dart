dd056817a6d2a936d0eb2c2e37c0f9ecbf48f9bd
Disha gupta <109867094+Disha152@users.noreply.github.com>
2023-01-07 16:50:05 +0530

    Resetting password with email

--- lib/pages/password_reset.dart
+++ lib/pages/password_reset.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_web/firebase_auth_web.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
 
@@ -11,47 +10,57 @@ class ForgotPasswordPage extends StatefulWidget {
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   final emailController = TextEditingController();
+
+
   @override
   void dispose() {
+    // ignore: todo
     // TODO: implement dispose
     emailController.dispose();
     super.dispose();
   }
 
-  void passwordReset() async {
-    try {
-      await FirebaseAuth.instance
-          .sendPasswordResetEmail(email: emailController.text.trim());
-    } catch (e) {
-      print(e);
-    }
+
+// ...
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+// ...
+
+Future<void> resetPassword(String email) async {
+  try {
+    await _auth.sendPasswordResetEmail(email: emailController.text.trim());
+    // Password reset email sent successfully
+  } catch (e) {
+    // An error occurred while trying to send the password reset email
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Forgot Password'),
+        title: const Text('Forgot Password'),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Container(
-          child: Column(
+      body: Column(
         children: [
-          Text('Please enter your register email'),
-          Container(
-              child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Email',
-              hintStyle: TextStyle(color: Colors.grey[400]),
-            ),
-            controller: emailController,
-          )),
-          MaterialButton(
-            onPressed: (() => passwordReset),
-            color: Colors.deepPurple,
-          )
+      const Text('Please enter your register email'),
+      TextField(
+        decoration: InputDecoration(
+      hintText: 'Email',
+      hintStyle: TextStyle(color: Colors.grey[400]),
+        ),
+        controller: emailController,
+      ),
+      MaterialButton(
+        onPressed: (() => resetPassword(emailController.text.trim())),
+        color: Colors.deepPurple,
+        child: Text("Reset Password")
+      )
         ],
-      )),
+      ),
     );
   }
 }
